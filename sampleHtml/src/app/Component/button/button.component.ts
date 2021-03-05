7cc5e87efd28fb71b06c5d0b796b4ce9c6a485df
oscar <oscar@enclave.vn>
2021-03-05 16:25:44 +0700

    add router

--- sampleHtml/src/app/Component/button/button.component.ts
+++ sampleHtml/src/app/Component/button/button.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-button',
@@ -8,8 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class ButtonComponent implements OnInit {
 
   constructor() { }
-
+  private router: Router
   ngOnInit(): void {
   }
-
 }
