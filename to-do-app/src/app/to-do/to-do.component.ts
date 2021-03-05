2448102212e806da1bbe8cdf76d33dba03e9f591
oscar <oscar@enclave.vn>
2021-03-05 09:01:08 +0700

    add animation

--- to-do-app/src/app/to-do/to-do.component.ts
+++ to-do-app/src/app/to-do/to-do.component.ts
@@ -1,9 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { animateChild, group, query, stagger, transition, trigger, useAnimation } from '@angular/animations';
+import { Component } from '@angular/core';
+import { moveDown, slideIn, slideOut } from '../animations';
 
 @Component({
   selector: 'app-to-do',
   templateUrl: './to-do.component.html',
-  styleUrls: ['./to-do.component.scss']
+  styleUrls: ['./to-do.component.scss'],
+  animations: [
+    trigger('todoAnimations', [
+      transition(':enter', [
+        group([
+          query('h1', [
+            useAnimation(moveDown)
+          ]),
+          query('input', [
+            useAnimation(moveDown)
+          ]),
+          query('@todoItem',
+            stagger(100, animateChild()))
+        ])
+      ])
+    ]),
+
+    trigger('todoItem', [
+      transition(':enter', [
+        useAnimation(slideIn),
+      ]),
+      transition(':leave', [
+        useAnimation(slideOut)
+      ]),
+    ])
+  ]
 })
 export class ToDoComponent  {
 
