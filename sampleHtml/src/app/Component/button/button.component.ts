a51fd74afb9b84f1e21d759948f85cf00c19766c
oscar <oscar@enclave.vn>
2021-03-09 08:35:30 +0700

    resolve conflict

--- sampleHtml/src/app/Component/button/button.component.ts
+++ sampleHtml/src/app/Component/button/button.component.ts
@@ -5,10 +5,7 @@ import { Router, RouterLink } from '@angular/router';
   selector: 'app-button',
   // styles : ["src/styles.css"],
   templateUrl: './button.component.html',
-<<<<<<< HEAD
   // styleUrls: ['./button.component.css']
-=======
->>>>>>> 30be659e8ccaa5202c38aa7c84b5752804301669
 })
 export class ButtonComponent implements OnInit {
   public firstName: string;
