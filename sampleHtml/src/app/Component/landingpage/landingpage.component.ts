fb26382a1312b8bf8de03c5c1c81956384402768
dangphucthinh <dangphucthinha9@gmail.com>
2021-03-25 21:35:07 +0700

    add awesome icons

--- sampleHtml/src/app/Component/landingpage/landingpage.component.ts
+++ sampleHtml/src/app/Component/landingpage/landingpage.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faBell, faCircle, faQuestionCircle } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-landingpage',
   templateUrl: './landingpage.component.html',
   styleUrls: ['./landingpage.component.css']
 })
 export class LandingpageComponent implements OnInit {
-
+  faFacebook = faFacebook;
+  faBell = faBell
+  faCircle = faQuestionCircle
+  faInstagram = faInstagram
   constructor() { }
 
   ngOnInit(): void {
